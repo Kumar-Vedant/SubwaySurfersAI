Fix PPO tensor gradient error

PPO/agent.py
@@ -35,17 +35,15 @@ class Agent:
         
         # get the probability distribution over actions and the value of this state
         dist, value = self.network(state)
-        # print(dist.probs.detach().cpu().numpy())
 
         # get an action by sampling the probability distribution and calculate the log-probability of the action taken
         action = dist.sample()
 
         prob = torch.squeeze(dist.log_prob(action)).item()
+        # prob = dist.log_prob(action).item()
         action = torch.squeeze(action).item()
-        # print(action)
-        
-        value = torch.squeeze(value).item()
 
+        value = torch.squeeze(value).item()
 
         return action, prob, value
 
@@ -96,7 +94,7 @@ class Agent:
         # calculate advantages and returns
         next_state = torch.tensor(np.array(next_state), dtype=torch.float32).unsqueeze(0).to(self.network.device)
         _, value = self.network(next_state)
-        value = value.squeeze()
+        value = value.squeeze().detach()
         values = torch.cat([values, value.unsqueeze(0)], dim=0)
 
         advantages = self.calculate_advantage(rewards, values, dones)

test.py
@@ -189,9 +189,9 @@ import cv2
 
 
 # loop over all elements in the folder "roll"
-# store_index = 21576
-# for i in range(21576):
-#     loaded_stack = np.load(f"./expert_data/no_op/obs_{i}.npy")
+# store_index = 3667
+# for i in range(3655):
+#     loaded_stack = np.load(f"./expert_data/right/obs_{i}.npy")
 
 #     # for each element, flip all 4 frames
 #     loaded_stack[0] = np.fliplr(loaded_stack[0])
@@ -203,7 +203,7 @@ import cv2
 #     image_stack = np.stack([loaded_stack[0], loaded_stack[1], loaded_stack[2], loaded_stack[3]], axis=0)
 
 #     # Save the stack to a .npy file
-#     save_path = f"/home/kumar-vedant/Documents/Development/subwaySurfersAI/expert_data/no_op/obs_{store_index}.npy"
+#     save_path = f"/home/kumar-vedant/Documents/Development/subwaySurfersAI/expert_data/left/obs_{store_index}.npy"
 #     np.save(save_path, image_stack)
 #     store_index += 1
 